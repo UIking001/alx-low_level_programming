e21bd9af692032ef3a35982c6c96f878f7a94d6f
Iheanyi <ukoiheanyikingz@gmail.com>
2022-10-18 12:33:01 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main -  prints Holberton, followed by a new line
+ * main - prints _putchar, followed by a new lin
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -15,6 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
+
 	return (0);
 }
